b135d142717bef6cb664bde233b10758a64593c0
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2021-08-04 22:50:38 +0300

    Add exercise related tables to the schema

--- schema.sql
+++ schema.sql
@@ -5,3 +5,31 @@ CREATE TABLE users (
     account_type INTEGER,
     visible BOOLEAN
 );
+
+CREATE TABLE exercises (
+    id SERIAL PRIMARY KEY,
+    title TEXT UNIQUE,
+    topic TEXT,
+    created_at TIMESTAMP,
+    visible BOOLEAN
+);
+
+CREATE TABLE words (
+    id SERIAL PRIMARY KEY,
+    exercise_id INTEGER REFERENCES exercises,
+    finnish_word TEXT,
+    swedish_word TEXT
+);
+
+CREATE TABLE answer_choices (
+    id SERIAL PRIMARY KEY,
+    word_id INTEGER REFERENCES words,
+    wrong_answer TEXT
+);
+
+CREATE TABLE answers (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    word_id INTEGER REFERENCES words,
+    result BOOLEAN
+);
