620d269ac84e2389971e2ad160c02f2cf98e65cc
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2021-08-19 11:58:43 +0300

    Add Bootstrap/JS validation to HypeZone messages

--- routes/messages.py
+++ routes/messages.py
@@ -4,14 +4,16 @@ from routes import helpers
 import messages
 import users
 
+MAX_MESSAGE_LEN = 100
 
 @app.route("/message/post", methods=["POST"])
 def post_message():
     helpers.abort_invalid_user_data()
 
     message = request.form["inputMessage"]
-
+    message = message.strip()
+    
     if message:
-        messages.add_message(users.get_logged_user_id(), message[:50])
+        messages.add_message(users.get_logged_user_id(), message[:MAX_MESSAGE_LEN])
 
     return redirect("/")
